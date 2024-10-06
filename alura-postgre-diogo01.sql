d737be9dc0ec8834229faff73c95b1aeec8858f3
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2024-10-06 20:17:46 -0300

    Finalização do curso

--- alura-postgre-diogo01.sql
+++ alura-postgre-diogo01.sql
@@ -174,5 +174,67 @@ SELECT
     SET id = 2
     WHERE id = 10;
 
+CREATE TABLE funcionarios(
+    id SERIAL PRIMARY KEY,
+    matricula VARCHAR(10),
+    nome VARCHAR(255),
+    sobrenome VARCHAR(255)
+);
 
-	
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M001', 'Diogo', 'Mascarenhas');
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M002', 'Vinícius', 'Dias');
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M003', 'Nico', 'Steppat');
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M004', 'João', 'Roberto');
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M005', 'Diogo', 'Mascarenhas');
+INSERT INTO funcionarios (matricula, nome, sobrenome) VALUES ('M006', 'Alberto', 'Martins');
+
+SELECT *
+FROM funcionarios
+ORDER BY 4 DESC, funcionarios.nome DESC, 2 ASC;
+
+
+SELECT 
+        aluno.id as aluno_id,
+        aluno.nome as "Nome do Aluno",
+        curso.id as "curso_id",
+        curso.nome as "Nome do Curso"
+    FROM aluno
+    JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+    JOIN curso ON curso.id = aluno_curso.curso_id
+    ORDER BY curso.nome, aluno.nome
+
+SELECT *
+  FROM funcionarios
+  ORDER BY id
+ LIMIT 5
+OFFSET 10;
+
+SELECT COUNT (id),
+       SUM(id),
+       MAX(id),
+       MIN(id),
+       ROUND(AVG(id),0)
+  FROM funcionarios;
+
+  SELECT curso.nome,
+        COUNT(aluno.id)
+    FROM aluno
+    JOIN aluno_curso ON aluno.id = aluno_curso.aluno_id
+    JOIN curso ON curso.id = aluno_curso.curso_id
+    GROUP BY 1
+    ORDER BY 1;
+
+
+	SELECT DISTINCT
+        nome
+  FROM funcionarios
+  ORDER BY nome;
+
+  SELECT curso.nome,
+        COUNT (aluno.id)
+    FROM curso
+    LEFT JOIN aluno_curso ON aluno_curso.curso_id = curso.id
+    LEFT JOIN aluno ON aluno.id = aluno_curso.aluno_id
+    --WHERE curso.nome = 'Javascritp'
+GROUP BY 1
+    HAVING COUNT (aluno.id) = 0
